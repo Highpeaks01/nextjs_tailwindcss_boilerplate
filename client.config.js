1b428e35afcd4e8054947c5716454bdc532020f0
highpeaks <twogheter.fun@gmail.com>
2025-03-21 09:44:46 +0100

    removed stuff

--- client.config.js
+++ client.config.js
@@ -1,71 +1,64 @@
-import { FaMicrophone } from "react-icons/fa"
-import { LuAudioLines } from "react-icons/lu"
-import { MdSpatialAudioOff } from "react-icons/md"
-
 const ClientConfig = {
 
-    appHeadline: "Your mentor on the line.",
-    appTitle: "Reach your goal faster with real-time hints and insights.",
+    appHeadline: "Headline.",
+    appTitle: "Title.",
     appDrophead: "",
 
     //API ENDPOINTS
     //Server
-    server_info: "https://backend.callvize.com/api/v1/server-info",
-
-    //Live call
-    call_audio: "https://backend.callvize.com/api/v1/call/audio",
+    server_info: "/api/v1/server-info",
 
     //Users
-    users_add: "https://backend.callvize.com/api/v1/users/add",
-    users_get: "https://backend.callvize.com/api/v1/users/get",
+    users_add: "/api/v1/users/add",
+    users_get: "/api/v1/users/get",
 
     //Calls
-    calls_add: "https://backend.callvize.com/api/v1/calls/add",
-    calls_edit: "https://backend.callvize.com/api/v1/calls/edit",
-    calls_get: "https://backend.callvize.com/api/v1/calls/get",
-    calls_delete: "https://backend.callvize.com/api/v1/calls/delete",
-    calls_duplicate: "https://backend.callvize.com/api/v1/calls/duplicate",
+    calls_add: "/api/v1/calls/add",
+    calls_edit: "/api/v1/calls/edit",
+    calls_get: "/api/v1/calls/get",
+    calls_delete: "/api/v1/calls/delete",
+    calls_duplicate: "/api/v1/calls/duplicate",
 
     //Subs
-    subs_add: "https://backend.callvize.com/api/v1/subs/add",
-    subs_get: "https://backend.callvize.com/api/v1/subs/get",
+    subs_add: "/api/v1/subs/add",
+    subs_get: "/api/v1/subs/get",
 
     //Misc
 
     
     //App data
-    appUrl: "https://talk.callvize.com",
-    email: "info@callvize.com",
-    linkedin: "https://www.linkedin.com/callvize",
-    calendly: "https://calendly.com/callvize/30min",
-    medium: "https://medium.com/@callvize",
-    appName: "Callvize",
+    appUrl: "https://url",
+    email: "email",
+    linkedin: "https://www.linkedin.com/name",
+    calendly: "https://calendly.com/name/30min",
+    medium: "https://medium.com/@name",
+    appName: "name",
 
     //Payments
-    stripePublic: "pk_live_51QyUaaGvBrN14sckgeKdfJbnghzA1ni7LXWcreM6ZwsJoA2PTCuoyWZQo2Fqv5IUgHrJNdm91u0SKu8z4sMkGkTJ001oTgv9tj",
-    stripePublicTest:"pk_test_51Qyd202fOUy6akPbOBdPZjXdhHoNNr8F91pqwgJGAFTlR6yOMSGXZi806lQL0vVgyWn3eRhyjcmfNps9Miq4hSEp00A7x0I5e5",
+    stripePublic: "",
+    stripePublicTest:"",
 
     //Products
     stripeProducts: [
         {
-            stripeId: "price_1QycUKGvBrN14sckTrC032Oi",
-            stripeIdTest: "price_1Qyd4B2fOUy6akPbgbZHt67V",
+            stripeId: "",
+            stripeIdTest: "",
             id: "starter",
             price: 7.99,
             hints: 500,
             duration: "1 month",
-            name: "Callvize Pro",
-            description: "Ideal for occasional callers"
+            name: "Pro",
+            description: ""
         },
         {
-            stripeId: "price_1QycV3GvBrN14sckYpxJHBwZ",
-            stripeIdTest: "price_1Qyd4Z2fOUy6akPbg1G2UzA0",
+            stripeId: "",
+            stripeIdTest: "",
             id: "pro",
             price: 17.99,
             hints: 2000,
             duration: "1 month",
-            name: "Callvize Pro",
-            description: "Perfect for heavy callers"
+            name: "",
+            description: ""
         },
     ]
 }
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,7 @@ import { getMessaging, isSupported } from "firebase/messaging"
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwYLNbZ7OyfFvDZi2P39rNKS9xvkEjqzU",
-  authDomain: "project0147584-553e2.firebaseapp.com",
-  projectId: "project0147584-553e2",
-  storageBucket: "project0147584-553e2.firebasestorage.app",
-  messagingSenderId: "204311492227",
-  appId: "1:204311492227:web:a090716e1d054150b3cdb6",
-  measurementId: "G-SE078W3EXJ"
+
 };
 
 // Initialize Firebase only once
